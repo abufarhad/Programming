e227fc1e1be0051e561de1254e9f5edbc0aab4f1
Md. Abu Farhad <abufarhad15@gmail.com>
2021-01-23 20:40:05 +0600

    commit

---
+++ Graphis OpenGL Codes/Line/Line_translation.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <GL/gl.h>
+#include <GL/glut.h>
+#include <GL/glu.h>
+#include <string>
+using namespace std;
+void init()
+{
+    glClearColor(0.50, .03, 1.0, 1.0);
+}
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    glLoadIdentity();
+
+    glBegin(GL_LINES);
+    glVertex2f(-100,0);
+    glVertex2f(100,0);
+    glVertex2f(0,100);
+    glVertex2f(0,-100);
+    glEnd();
+
+    glBegin(GL_LINES);
+    glVertex2f(15,50);
+    glVertex2f(50,15);
+    glEnd();
+
+    glTranslatef(20,30,0);
+    glBegin(GL_LINES);
+    glVertex2f(15,50);
+    glVertex2f(50,15);
+    glEnd();
+
+    glutSwapBuffers();
+}
+
+void reshape(int w, int h)
+{
+    glViewport(0,0,(GLsizei)w,(GLsizei)h);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluOrtho2D(-100,100,-100,100);
+    glMatrixMode(GL_MODELVIEW);
+}
+
+int main(int argc, char *argv[])
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
+    glutInitWindowPosition(300,100);
+    glutInitWindowSize(900,600);
+    glutCreateWindow("Line Translation");
+
+    glutDisplayFunc(display);
+    glutReshapeFunc(reshape);
+    init();
+    glutMainLoop();
+
+    return 0;
+}
